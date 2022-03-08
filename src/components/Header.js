4f2758fd218dbef80fc810b0586f3dd1cfa8ff5a
Josh <oreztnega1996@gmail.com>
2022-03-08 11:51:43 +0800

    Fixed header styling alignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,8 +12,10 @@ export class Header extends Component {
 
     return (
       <div className="border-bottom">
-        <div className="d-flex align-items-center">
-          <div className="py-2 px-3 border-end">Narwhal</div>
+        <div className="d-flex">
+          <div className="py-2 px-3 border-end d-flex justify-content-center align-items-center">
+            Narwhal
+          </div>
           <div className="p-2 flex-grow-1 d-flex justify-content-between align-items-center">
             <p className="mb-0">{capitalizedTopPath}</p>
             <div className="d-flex align-items-center">
