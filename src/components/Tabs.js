3d74c6ef58d7beca59c85c67b4b5ac8d7d63f479
Josh <oreztnega1996@gmail.com>
2022-03-17 18:20:12 +0800

    Added hackish fix for Tabs.js underline in production

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -11,6 +11,15 @@ export class Tabs extends Component {
     };
   }
 
+  componentDidMount() {
+    // Hack-ish fix in production. For some reason, the underline doesnt work in product
+    // But works in dev mode. Seems like the DOMNode's width is unreliable on mount or smth.
+    setTimeout(() => {
+      this.refCallbackCalledBefore = true;
+      this.actualRefCallback(this.selectedDomNode);
+    }, 0);
+  }
+
   tabItem = (text, isSelected, pathLink, key) => {
     return (
       <Link
@@ -27,8 +36,15 @@ export class Tabs extends Component {
     );
   };
 
-  // REFLECTION: Find out exactly how refs work
   refCallback = (node) => {
+    this.selectedDomNode = node;
+    if (this.refCallbackCalledBefore) {
+      this.actualRefCallback(this.selectedDomNode);
+    }
+  };
+
+  // REFLECTION: Find out exactly how refs work
+  actualRefCallback = (node) => {
     if (node) {
       this.setState({
         underlineLeft: node.offsetLeft,
@@ -44,7 +60,7 @@ export class Tabs extends Component {
           {this.props.tabs.map((tab, index) =>
             this.tabItem(
               tab.text,
-              this.props.selected == tab,
+              this.props.selected === tab,
               tab.pathLink,
               index
             )
